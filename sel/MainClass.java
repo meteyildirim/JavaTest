7028fa2ca3008b2acb82dc8240af59e418120093
Mete Yildirim <Mete.Yildirim@boc-group.com>
2022-07-04 22:14:00 +0200

    Creating of sel and testNg

---
+++ sel/MainClass.java
@@ -0,0 +1,10 @@
+package sel;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testNg/MainClass.java
@@ -0,0 +1,10 @@
+package testNg;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
